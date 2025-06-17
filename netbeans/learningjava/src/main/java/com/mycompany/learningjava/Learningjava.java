54e8726963e44f8f3e6063e4359b39dda65e0b7f
Atilla Gunal <atilla.gunal@gmail.com>
2025-06-17 10:26:07 +0300

    array list usage

--- netbeans/learningjava/src/main/java/com/mycompany/learningjava/Learningjava.java
+++ netbeans/learningjava/src/main/java/com/mycompany/learningjava/Learningjava.java
@@ -5,6 +5,7 @@
 package com.mycompany.learningjava;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  *
@@ -14,13 +15,24 @@ public class Learningjava {
 
     public static void main(String[] args)
     {
+        /*
         System.out.println("Hello World!");
-        
         System.out.println("Enter your name:");
         
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         
         System.out.println("Hello " + name);
+        */
+        
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.remove(3);
+                
+        System.out.println(numbers);
     }
 }
